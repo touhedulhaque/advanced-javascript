d585c1b8fdd742f237420917b55da28bab300119
touhedulhaque <razmbamkt@gmail.com>
2020-08-09 09:08:51 +0200

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+console.log(pakhi)
+
+
+
+function add(num1, num2){
+    console.log(num1+num2);
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+const bf = {name:'Tom erick', tel: 9987654};
+console.log(bf.gf);
+
