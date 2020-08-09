932e0ae32af7d6b9f4bbcebd8a4ccbf4469ec703
touhedulhaque <razmbamkt@gmail.com>
2020-08-09 10:21:11 +0200

    all push

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,7 @@
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("true")
+} else{
+    console.log("false")
+}
---
+++ object-property.js
@@ -0,0 +1,24 @@
+const students = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 22, name: 'Salman Shah'},
+    {id: 23, name: 'Sakib Khan'},
+    {id: 24, name: 'Sanju John'},
+    {id: 25, name: 'Riaz Khan'},
+    {id: 26, name: 'Manna'},
+    {id: 27, name: 'Amit Hasan'}
+]
+
+const studentName = [];
+
+for (let i = 0; i < students.name; i++) {
+    const result = students.name[i];
+    output.push(result)
+    
+}
+
+const names = students.map( s => s.name);
+const ids = students.map( s => s.id);
+
+const bigger = students.filter ( s => s.id>25);
+const biggerOne = students.find ( s=> s.id>25)
+console.log(biggerOne);
